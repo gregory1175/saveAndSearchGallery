b041df2cae68f6de71a0a5891bd9083d5bb59411
gregory1175 <151869985+gregory1175@users.noreply.github.com>
2024-09-08 19:28:14 +0300

    Add docker + connect mondobd express

--- api/main.py
+++ api/main.py
@@ -3,6 +3,10 @@ import requests
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from flask_cors import CORS
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery 
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -21,7 +25,22 @@ CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+# connect with database 
+@app.route("/images", methods=["GET","POST"])
+def images():
+    if request.method == "GET":
+        # read images from database 
+         images  = images_collection.find({})
+         return jsonify([img for img in images])
+    if request.method == "POST":
+        # save image in the database 
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
+# connect with frontend
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
